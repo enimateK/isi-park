487942020afe5cabaad9eebfeb319de4de1c37ae
Bowmii <motard.bastien@gmail.com>
2019-01-11 17:09:58 +0100

    Commit de départ sur branche perso

--- app/src/main/java/org/appli/bastien/isi_park/MainActivity.java
+++ app/src/main/java/org/appli/bastien/isi_park/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.ListView;
 
 import org.appli.bastien.isi_park.model.Parking;
+import org.appli.bastien.isi_park.ui.ParkingAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,7 +25,11 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        List<Parking> ListParking = new ArrayList<Parking>();
+        ParkingAdapter m_ParkingAdapter;
+
+        m_ParkingAdapter = new ParkingAdapter(this, new ArrayList<Parking>());
+
+        m_ListParking.setAdapter(m_ParkingAdapter);
 
 
         
--- app/src/main/java/org/appli/bastien/isi_park/ui/ParkingAdapter.java
+++ app/src/main/java/org/appli/bastien/isi_park/ui/ParkingAdapter.java
@@ -1,15 +1,41 @@
 package org.appli.bastien.isi_park.ui;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import org.appli.bastien.isi_park.R;
+import org.appli.bastien.isi_park.model.Parking;
 
-public class ParkingAdapter extends AppCompatActivity {
+import java.util.List;
 
+public class ParkingAdapter extends ArrayAdapter<Parking> {
+
+
+    public ParkingAdapter (Context context, List<Parking> park){
+        super(context, -1, park);
+    }
+
+    @NonNull
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_parking_adapter);
+
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View actualView = convertView;
+
+        if (convertView == null) {
+
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            actualView = inflater.inflate(R.layout.activity_parking_adapter, parent, false);
+
+        }
+        return actualView;
     }
 }
